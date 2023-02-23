48f1c58f86264d26a13289f11c81f6712e238ee6
18anguyen24 <andrew.nguyen424@gmail.com>
2023-02-23 12:19:55 -0800

    change data type in h

--- bin/wait/Wait.h
+++ bin/wait/Wait.h
@@ -38,7 +38,7 @@ class Wait : public POSIXApplication
      * @param argc Argument count
      * @param argv Argument values
      */
-    Wait(int argc);
+    Wait(pid_t argc);
 
     /**
      * Destructor
