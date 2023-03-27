8abbfab57e8fb33c9c6c46b7faa2efb9130d90a2
18anguyen24 <andrew.nguyen424@gmail.com>
2023-03-26 19:06:42 -0700

    tt

--- lib/libruntime/ProcessClient.cpp
+++ lib/libruntime/ProcessClient.cpp
@@ -41,11 +41,11 @@ Priority ProcessClient::getProcessPriority() const
 {
     return m_priority;
 }
-
+/*
 Priority ProcessClient::setProcessPriority(const Priority prio) const
 {
     m_priority = prio;
-}
+}*/
 
 ProcessClient::Result ProcessClient::processInfo(const ProcessID pid,
                                                  ProcessClient::Info &info) const
