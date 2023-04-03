98114407b7408aa63d447a9d24ca34b215b4718d
hnguyen07 <54465701+hnguyen07@users.noreply.github.com>
2023-04-03 15:38:12 -0700

    Updated the data type of priority to u8 instead of u 32 (for storage efficiency)

--- lib/libstd/Types.h
+++ lib/libstd/Types.h
@@ -139,7 +139,7 @@ typedef unsigned short GroupID;
 /** Process Identification Number. */
 typedef u32 ProcessID;
 
-typedef u32 Priority;
+typedef u8 Priority;
 
 /**
  * Describes a device ID number.
